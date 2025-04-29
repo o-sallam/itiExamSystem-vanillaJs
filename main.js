f70d14026936a7415a06a41052c43fcabbe13d00
Mohammad Mansour <muhammadmansour010@gmail.com>
2025-04-29 16:25:43 +0300

    feat:first page exam system

---
+++ main.js
@@ -0,0 +1,19 @@
+let signUpBtn = document.getElementById("signUpBtn");
+let signInBtn = document.getElementById("signInBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+signInBtn.onclick = function(){
+    nameField.style.maxHeight="0";
+    title.innerHTML="Sign In";
+    signUpBtn.classList.add("disabled")
+    signInBtn.classList.remove("disabled")
+
+}
+signUpBtn.onclick = function(){
+    nameField.style.maxHeight="65px";
+    title.innerHTML="Sign Up";
+    signUpBtn.classList.remove("disabled");
+    signInBtn.classList.add("disabled");
+
+}
